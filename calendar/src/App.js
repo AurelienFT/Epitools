be6cc5e54f49af1e97d95aba3c32ce34ac4103f7
Aurelien Foucault <aurelien.foucault@epitech.eu>
2020-02-24 20:22:51 +0100

    Remove tokens

--- calendar/src/App.js
+++ calendar/src/App.js
@@ -2,17 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { GoogleLogin } from 'react-google-login';
+import MicrosoftLogin from "react-microsoft-login";
+
+function authHandler(err, data) {
+  console.log(err, data);
+};
 
 function responseGoogle(response) {
   console.log(response)
 }
 
 function App() {
+  console.log(process.env.REACT_APP_CLIENT_ID)
   return (
     <div className="App">
       <header className="App-header">
         <GoogleLogin
-          clientId="589355162-su0mdr3b8abaj2fgvl6uebtbha07u6t3.apps.googleusercontent.com"
+          clientId={process.env.REACT_APP_CLIENT_ID}
           buttonText="Login"
           onSuccess={responseGoogle}
           onFailure={responseGoogle}
